d3d4232a709481d0dbd6a53c002e9a75e2ff6a52
Username <codrinaignat@hotmail.com>
2023-10-08 12:07:46 +0300

    Rename some functions

--- Problem11.py
+++ Problem11.py
@@ -1,9 +1,9 @@
 from itertools import groupby
-def encode_modified(alist):
-        def aux(lg):
-            if len(lg)>1: return [len(lg), lg[0]]
-            else: return lg[0]
-        return [aux(list(group)) for key, group in groupby(alist)]
+def modified_encode(aList):
+        def second_fcn(length):
+            if len(length)>1: return [len(length), length[0]]
+            else: return length[0]
+        return [second_fcn(list(group)) for key, group in groupby(aList)]
 
 aList = [0, 0, 1, 2, 2, 3, 4, 5, 5, 6, 7, 7, 9]
-print(encode_modified(aList))
+print(modified_encode(aList))
