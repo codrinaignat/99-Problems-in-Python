e871d132b6e75b716acad3d27689eaad1454a062
Username <codrinaignat@hotmail.com>
2023-10-14 08:55:09 +0300

    Modify list

--- Problem15.py
+++ Problem15.py
@@ -3,5 +3,5 @@
 def duplicate(myList, N):
     return [item for item in myList for i in range(N)]
 
-myList = myList = [0, 1, 2, 3, 4, 5, 6]
+myList = [0, 1, 2, 3, 4, 5, 6]
 print(duplicate(myList, 3))
