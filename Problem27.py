ba3bdc71c7b15368b7526fec6b164ac3b76d2f6b
Username <codrinaignat@hotmail.com>
2023-11-09 20:02:25 +0200

    Problem modifications

--- Problem27.py
+++ Problem27.py
@@ -7,22 +7,23 @@ def disjointSubsets(setOfElements, setOfSubsets):
     numberOfGroups = len(setOfSubsets)
     for i in range(0, numberOfGroups):
         sumOfElements = sumOfElements + setOfSubsets[i]
-    print(sumOfElements)
-    print(numberOfGroups)
+    #print(sumOfElements)
+    #print(numberOfGroups)
 
     setOfElementsList = list(setOfElements)
-    print(setOfElementsList)
+    #print(setOfElementsList)
 
     if len(setOfElementsList) == sumOfElements:
-        for j in setOfSubsets:
+        for j in range(0, numberOfGroups):
             subsets = list(itertools.combinations(setOfElementsList, setOfSubsets[j]))
             print(subsets)
     else:
         print("The number of elements in set does not match the sum of subsets array elements")
     subsetsListBackToSet = set(subsets)
-    print(subsetsListBackToSet)
+    #print(subsetsListBackToSet)
     return subsetsListBackToSet
 
-mySet = {"apples", "strawberries", "pears", "cherries", "bananas", "plums", "watermelons"}
-mySets = [2, 3, 2]
-print(disjointSubsets(mySet, mySets))
+mySet = {"banane", "pere", "mere", "gutui"}
+subsetLengths = [1, 2, 1]
+print("Here the program executed again:")
+print(disjointSubsets(mySet, subsetLengths))
