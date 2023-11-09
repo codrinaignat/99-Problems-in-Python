e56f4ee4cb282f7e40beaaaeed3c615365875593
Codrina Lisaru <94629883+codrinaignat@users.noreply.github.com>
2023-11-09 17:58:39 +0200

    Update Problem27.py

--- Problem27.py
+++ Problem27.py
@@ -1,21 +1,28 @@
 #Group the elements of a set into disjoint subsets.
+import itertools
+
+def disjointSubsets(setOfElements, setOfSubsets):
+    
+    sumOfElements = 0
+    numberOfGroups = len(setOfSubsets)
+    for i in range(0, numberOfGroups):
+        sumOfElements = sumOfElements + setOfSubsets[i]
+    print(sumOfElements)
+    print(numberOfGroups)
+
+    setOfElementsList = list(setOfElements)
+    print(setOfElementsList)
+
+    if len(setOfElementsList) == sumOfElements:
+        for j in setOfSubsets:
+            subsets = list(itertools.combinations(setOfElementsList, setOfSubsets[j]))
+            print(subsets)
+    else:
+        print("The number of elements in set does not match the sum of subsets array elements")
+    subsetsListBackToSet = set(subsets)
+    print(subsetsListBackToSet)
+    return subsetsListBackToSet
 
 mySet = {"apples", "strawberries", "pears", "cherries", "bananas", "plums", "watermelons"}
 mySets = [2, 3, 2]
-
-def disjointSubsets (setOfElements, setOfSubsets):
-    sumOfElements = 0
-    numberOfGroups = 0
-    for i in range(0, len(setOfSubsets)):
-        sumOfElements = sumOfElements + setOfSubsets[i]
-        numberOfGroups = numberOfGroups + 1
-    print(sumOfElements)
-    print(numberOfGroups)
-        
-    if len(setOfElements) == sumOfElements:
-        for j in range(0, numberOfGroups):
-            #split here the set into subsets
-    else:
-        print("The number of elements in set does not match the sum of subsets array elements")
-        
 print(disjointSubsets(mySet, mySets))
