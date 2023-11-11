84d564321d78717eaf111083116220f7019d9ec9
Codrina Lisaru <94629883+codrinaignat@users.noreply.github.com>
2023-11-11 21:15:04 +0200

    Update Problem27.py

--- Problem27.py
+++ Problem27.py
@@ -3,18 +3,22 @@ import itertools
 def disjointSubsets(setOfElements, setOfSubsets):
     sumOfElements = 0
     numberOfGroups = len(setOfSubsets)
+    x = list()
     for i in range(0, numberOfGroups):
         sumOfElements = sumOfElements + setOfSubsets[i]
     setOfElementsList = list(setOfElements)
     if len(setOfElementsList) == sumOfElements:
         for j in range(0, numberOfGroups):
             subsets = list(itertools.combinations(setOfElementsList, setOfSubsets[j]))
+            x.append(subsets)
             subsetsListBackToSet = set(subsets)
             print(subsetsListBackToSet)
+            
     else:
         print("The number of elements in set does not match the sum of subsets array elements")
-    return 
+    print(x)
+    return subsetsListBackToSet
 
-mySet = {"aldo", "beat", "carla", "david", "ivan"}
-subsetLengths = [2, 3]
-print(disjointSubsets(mySet, subsetLengths))
+mySet = {"gigi", "carla", "david", "gutsy"}
+subsetLengths = [2, 1, 1]
+disjointSubsets(mySet, subsetLengths)
