37edbb9b4bead5ce6eb933d2b09cfca78b78677d
Username <codrinaignat@hotmail.com>
2024-01-12 15:20:20 +0200

    Solve problem 32

--- Problem32.py
+++ Problem32.py
@@ -6,3 +6,12 @@
 
 # c = math.gcd(a, b)
 # print(c)
+
+a = int(input("First number "))
+b = int(input("Second number ")) 
+remainder=a%b
+while remainder:
+    a = b
+    b = remainder
+    remainder = a%b
+print('GCD is:', b)
